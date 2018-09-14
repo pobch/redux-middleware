31e0c112a28e86683b9b7b753245e99d8fda447f
Pob <590650@gmail.com>
2018-09-14 19:04:07 +0700

    finished stateValidator middleware

--- src/Root.js
+++ src/Root.js
@@ -3,11 +3,16 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import PropTypes from 'prop-types'
 import async from './middlewares/async'
+import stateValidator from './middlewares/stateValidator'
 import reducers from './reducers'
 
 const Root = props => {
   const { children, initialState } = props
-  const store = createStore(reducers, initialState, applyMiddleware(async))
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  )
 
   return <Provider store={store}>{children}</Provider>
 }
